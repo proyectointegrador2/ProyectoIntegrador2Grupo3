3f2b4534c28bf1b0b4f3abdbed2fb6d6fbd58049
robertpena6498 <robertpena6498@gmail.com>
2023-02-22 14:37:41 -0400

    Aqui esta las pantalla de agregar un vehiculo

--- ClientApp/src/components/DesignAddCar/AddProduct.js
+++ ClientApp/src/components/DesignAddCar/AddProduct.js
@@ -1,95 +1,223 @@
 ﻿import React, { useState } from 'react';
 import {
-    Carousel,
-    CarouselItem,
-    CarouselControl,
-    CarouselIndicators,
-    CarouselCaption,
+    Form,
+    Row,
+    Col,
+    FormGroup,
+    Label,
+    Input,
+    Button,
 } from 'reactstrap';
 
-const items = [
-    {
-        src: 'https://picsum.photos/id/123/1200/400',
-        altText: 'Slide 1',
-        caption: 'Slide 1',
-        key: 1,
-    },
-    {
-        src: 'https://picsum.photos/id/456/1200/400',
-        altText: 'Slide 2',
-        caption: 'Slide 2',
-        key: 2,
-    },
-    {
-        src: 'https://picsum.photos/id/678/1200/400',
-        altText: 'Slide 3',
-        caption: 'Slide 3',
-        key: 3,
-    },
-];
 
 function AddProduct(args) {
-    const [activeIndex, setActiveIndex] = useState(0);
-    const [animating, setAnimating] = useState(false);
+    return <Form>
+        <Row>
+            <Col md={5}>
+                <FormGroup>
+                    <Label for="Nombre">
+                        Nombre
+                    </Label>
+                    <Input
+                        id="exampleEmail"
+                        name="email"
+                      
+                        type="email"
+                    />
+                </FormGroup>
+            </Col>
+            <Col md={5}>
+                <FormGroup>
+                    <Label for="examplePassword">
+                        Modelo
+                    </Label>
+                    <Input
+                        id="examplePassword"
+                        name="password"
+                       
+                        type="password"
+                    />
+                </FormGroup>
+            </Col>
+            <Col md={5}>
+                <FormGroup>
+                    <Label for="examplePassword">
+                        Marca
+                    </Label>
+                    <Input
+                        id="examplePassword"
+                        name="password"
 
-    const next = () => {
-        if (animating) return;
-        const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-        setActiveIndex(nextIndex);
-    };
+                        type="password"
+                    />
+                </FormGroup>
+            </Col>
+            <Col md={5}>
+                <FormGroup>
+                    <Label for="examplePassword">
+                        Chasis
+                    </Label>
+                    <Input
+                        id="examplePassword"
+                        name="password"
 
-    const previous = () => {
-        if (animating) return;
-        const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-        setActiveIndex(nextIndex);
-    };
+                        type="password"
+                    />
+                </FormGroup>
+            </Col>
+            <Col md={5}>
+                <FormGroup>
+                    <Label for="examplePassword">
+                        Placa
+                    </Label>
+                    <Input
+                        id="examplePassword"
+                        name="password"
 
-    const goToIndex = (newIndex) => {
-        if (animating) return;
-        setActiveIndex(newIndex);
-    };
+                        type="password"
+                    />
+                </FormGroup>
+            </Col>
+            <Col md={5}>
+                <FormGroup>
+                    <Label for="examplePassword">
+                        Año
+                    </Label>
+                    <Input
+                        id="examplePassword"
+                        name="password"
 
-    const slides = items.map((item) => {
-        return (
-            <CarouselItem
-                onExiting={() => setAnimating(true)}
-                onExited={() => setAnimating(false)}
-                key={item.src}
+                        type="password"
+                    />
+                </FormGroup>
+            </Col>
+            <Col md={5}>
+                <FormGroup>
+                    <Label for="examplePassword">
+                        Color
+                    </Label>
+                    <Input
+                        id="examplePassword"
+                        name="password"
+
+                        type="password"
+                    />
+                </FormGroup>
+            </Col>
+            <Col md={5}>
+                <FormGroup>
+                    <Label for="examplePassword">
+                        Cilindraje
+                    </Label>
+                    <Input
+                        id="examplePassword"
+                        name="password"
+
+                        type="password"
+                    />
+                </FormGroup>
+            </Col>
+            <Col md={5}>
+                <FormGroup>
+                    <Label for="examplePassword">
+                        Kilometro en Tablero
+                    </Label>
+                    <Input
+                        id="examplePassword"
+                        name="password"
+
+                        type="password"
+                    />
+                </FormGroup>
+            </Col>
+            <Col md={5}>
+                <FormGroup>
+                    <Label for="examplePassword">
+                        Numero de puertas
+                    </Label>
+                    <Input
+                        id="examplePassword"
+                        name="password"
+
+                        type="password"
+                    />
+                </FormGroup>
+            </Col>
+            <Col md={5}>
+                <FormGroup>
+                    <Label for="examplePassword">
+                        Precio
+                    </Label>
+                    <Input
+                        id="examplePassword"
+                        name="password"
+
+                        type="password"
+                    />
+                </FormGroup>
+            </Col>
+            <Col md={5}>
+                <FormGroup>
+                    <Label for="examplePassword">
+                        Stock
+                    </Label>
+                    <Input
+                        id="examplePassword"
+                        name="password"
+
+                        type="password"
+                    />
+                </FormGroup>
+            </Col>
+            <Col md={5}>
+                <FormGroup>
+                    <Label for="examplePassword">
+                        Fecha de registro
+                    </Label>
+                    <Input
+                        id="examplePassword"
+                        name="password"
+
+                        type="password"
+                    />
+                </FormGroup>
+            </Col>
+            <Col md={5}>
+                <FormGroup>
+                    <Label for="examplePassword">
+                        Fecha de modificación
+                    </Label>
+                    <Input
+                        id="examplePassword"
+                        name="password"
+
+                        type="password"
+                    />
+                </FormGroup>
+            </Col>
+            
+           
+        </Row>
+       
+            
+        <FormGroup check>
+            <Input
+                id="exampleCheck"
+                name="check"
+                type="checkbox"
+            />
+            <Label
+                check
+                for="exampleCheck"
             >
-                <img src={item.src} alt={item.altText} />
-                <CarouselCaption
-                    captionText={item.caption}
-                    captionHeader={item.caption}
-                />
-            </CarouselItem>
-        );
-    });
-
-    return (
-        <Carousel
-            activeIndex={activeIndex}
-            next={next}
-            previous={previous}
-            {...args}
-        >
-            <CarouselIndicators
-                items={items}
-                activeIndex={activeIndex}
-                onClickHandler={goToIndex}
-            />
-            {slides}
-            <CarouselControl
-                direction="prev"
-                directionText="Previous"
-                onClickHandler={previous}
-            />
-            <CarouselControl
-                direction="next"
-                directionText="Next"
-                onClickHandler={next}
-            />
-        </Carousel>
-    );
+                Confirmar
+            </Label>
+        </FormGroup>
+        <Button>
+            Agregar Vehiculo
+        </Button>
+    </Form>
+    
 }
 
 export default AddProduct;
