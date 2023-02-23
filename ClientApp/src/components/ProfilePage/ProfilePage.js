b09043522226a34dfd5fab0ab22d0a17ebb7b0e1
robertpena6498 <robertpena6498@gmail.com>
2023-02-23 18:58:36 -0400

    Aqui esta el diseño inicial del editor de Perfil

--- ClientApp/src/components/ProfilePage/ProfilePage.js
+++ ClientApp/src/components/ProfilePage/ProfilePage.js
@@ -16,115 +16,61 @@ function ProfilePage() {
     return (<Form>
         <FormGroup>
             <Label for="exampleEmail">
-                Plain Text (Static)
+                Perfil
             </Label>
             <Input
                 plaintext
-                value="Some plain text/ static value"
+                value="Nombre de Perfil"
             />
         </FormGroup>
         <FormGroup>
             <Label for="exampleEmail">
-                Email
+                Correo Electronico
             </Label>
             <Input
                 id="exampleEmail"
                 name="email"
-                placeholder="with a placeholder"
+               
                 type="email"
             />
         </FormGroup>
         <FormGroup>
             <Label for="examplePassword">
-                Password
+                Usuario 
             </Label>
             <Input
                 id="examplePassword"
                 name="password"
-                placeholder="password placeholder"
+                
                 type="password"
             />
         </FormGroup>
         <FormGroup>
             <Label for="exampleUrl">
-                Url
+                Contraseña
             </Label>
             <Input
                 id="exampleUrl"
                 name="url"
-                placeholder="url placeholder"
+                
                 type="url"
             />
         </FormGroup>
         <FormGroup>
             <Label for="exampleNumber">
-                Number
+                Numero telefonico
             </Label>
             <Input
                 id="exampleNumber"
                 name="number"
-                placeholder="number placeholder"
+                
                 type="number"
             />
         </FormGroup>
-        <FormGroup>
-            <Label for="exampleDatetime">
-                Datetime
-            </Label>
-            <Input
-                id="exampleDatetime"
-                name="datetime"
-                placeholder="datetime placeholder"
-                type="datetime"
-            />
-        </FormGroup>
-        <FormGroup>
-            <Label for="exampleDate">
-                Date
-            </Label>
-            <Input
-                id="exampleDate"
-                name="date"
-                placeholder="date placeholder"
-                type="date"
-            />
-        </FormGroup>
-        <FormGroup>
-            <Label for="exampleTime">
-                Time
-            </Label>
-            <Input
-                id="exampleTime"
-                name="time"
-                placeholder="time placeholder"
-                type="time"
-            />
-        </FormGroup>
-        <FormGroup>
-            <Label for="exampleColor">
-                Color
-            </Label>
-            <Input
-                id="exampleColor"
-                name="color"
-                placeholder="color placeholder"
-                type="color"
-            />
-        </FormGroup>
-        <FormGroup>
-            <Label for="exampleSearch">
-                Search
-            </Label>
-            <Input
-                id="exampleSearch"
-                name="search"
-                placeholder="search placeholder"
-                type="search"
-            />
-        </FormGroup>
+        
         <FormGroup>
             <Label for="exampleSelect">
-                Select
+                Sexo
             </Label>
             <Input
                 id="exampleSelect"
@@ -132,93 +78,21 @@ function ProfilePage() {
                 type="select"
             >
                 <option>
-                    1
+                    Masculino
                 </option>
                 <option>
-                    2
+                    Femenino
                 </option>
                 <option>
-                    3
+                    No difinido
                 </option>
-                <option>
-                    4
-                </option>
-                <option>
-                    5
-                </option>
+               
             </Input>
-        </FormGroup>
-        <FormGroup>
-            <Label for="exampleSelectMulti">
-                Select Multiple
-            </Label>
-            <Input
-                id="exampleSelectMulti"
-                multiple
-                name="selectMulti"
-                type="select"
-            >
-                <option>
-                    1
-                </option>
-                <option>
-                    2
-                </option>
-                <option>
-                    3
-                </option>
-                <option>
-                    4
-                </option>
-                <option>
-                    5
-                </option>
-            </Input>
-        </FormGroup>
-        <FormGroup>
-            <Label for="exampleText">
-                Text Area
-            </Label>
-            <Input
-                id="exampleText"
-                name="text"
-                type="textarea"
-            />
-        </FormGroup>
-        <FormGroup>
-            <Label for="exampleFile">
-                File
-            </Label>
-            <Input
-                id="exampleFile"
-                name="file"
-                type="file"
-            />
-            <FormText>
-                This is some placeholder block-level help text for the above input. It‘s a bit lighter and easily wraps to a new line.
-            </FormText>
-        </FormGroup>
-        <FormGroup>
-            <Label for="exampleRange">
-                Range
-            </Label>
-            <Input
-                id="exampleRange"
-                name="range"
-                type="range"
-            />
-        </FormGroup>
-        <FormGroup check>
-            <Input type="radio" />
-            {' '}Option one is this and that—be sure to
-            <Label check>
-                include why it‘s great
-            </Label>
         </FormGroup>
         <FormGroup check>
             <Input type="checkbox" />
             <Label check>
-                Check me out
+                Confirmar
             </Label>
         </FormGroup>
     </Form>)
