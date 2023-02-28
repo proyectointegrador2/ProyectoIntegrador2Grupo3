b6809489650fbdd83a63f1f14e4c205e07945412
robertpena6498 <robertpena6498@gmail.com>
2023-02-28 19:42:06 -0400

    El formulario del Edit User completado.

--- ClientApp/src/components/EditUser/EditUser.js
+++ ClientApp/src/components/EditUser/EditUser.js
@@ -1,54 +1,126 @@
 ﻿import React, { useState } from 'react';
 import {
-    Form,
-    Input,
-    option,
-    InputGroup,
+Form,
+FormGroup,
+Label,
+Input,
+Button,
+
+
+
 } from 'reactstrap';
 
+function EditUser() {
+    return (<Form>
+        <FormGroup>
+            <Label for="exampleEmail">
+                Editor de Usario
+            </Label>
+            <Input
+                plaintext
+                value="Nombre de Perfil"
+            />
+        </FormGroup>
+        <FormGroup>
+            <Label for="exampleEmail">
+                Correo Electronico
+            </Label>
+            <Input
+                id="exampleEmail"
+                name="email"
 
- function EditUser() {
-     return (<Form>
+                type="email"
+            />
+        </FormGroup>
+        <FormGroup>
+            <Label for="exampleDirección">
+                Dirección
+            </Label>
+            <Input
+                id="examplDirección"
+                name="Dirección"
 
-         <InputGroup> 
+                
+            />
+        </FormGroup>
+        <FormGroup>
+            <Label for="examplePassword">
+                Nombre de Usuario
+            </Label>
+            <Input
+                id="examplePassword"
+                name="password"
 
-        </InputGroup>
-        
-        <Input
-            className="mb-3"
-            placeholder="default"
-        />
-        <Input
-            bsSize="sm"
-            className="mb-3"
-            placeholder="sm"
-        />
-        <Input
-            bsSize="lg"
-            className="mb-3"
-            type="select"
-        >
-            <option>
-                Large Select
-            </option>
-        </Input>
-        <Input
-            className="mb-3"
-            type="select"
-        >
-            <option>
-                Default Select
-            </option>
-        </Input>
-        <Input
-            bsSize="sm"
-            className="mb-3"
-            type="select"
-        >
-            <option>
-                Small Select
-            </option>
-        </Input>
+                type="password"
+            />
+        </FormGroup>
+        <FormGroup>
+            <Label for="exampleUrl">
+                Contraseña
+            </Label>
+            <Input
+                id="exampleUrl"
+                name="url"
+
+                type="url"
+            />
+        </FormGroup>
+        <FormGroup>
+            <Label for="exampleNumber">
+                Numero telefonico
+            </Label>
+            <Input
+                id="exampleNumber"
+                name="number"
+
+                type="number"
+            />
+        </FormGroup>
+
+        <FormGroup>
+            <Label for="exampleSelect">
+                Sexo
+            </Label>
+            <Input
+                id="exampleSelect"
+                name="select"
+                type="select"
+            >
+                <option>
+                    Masculino
+                </option>
+                <option>
+                    Femenino
+                </option>
+                <option>
+                    No difinido
+                </option>
+
+            </Input>
+        </FormGroup>
+        <FormGroup>
+            <Label for="exampleSelect">
+                Rol de usuario
+            </Label>
+            <Input
+                id="exampleSelect"
+                name="select"
+                type="select"
+            >
+                <option>
+                    Administrador
+                </option>
+                <option>
+                    Cliente
+                </option>
+            
+
+            </Input>
+        </FormGroup>
+
+        <Button type="submit">
+            Modificar Usuario
+        </Button>
     </Form>)
 }
 
