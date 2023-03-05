484abdba2d44358bac816a1ac282a94bf7badebf
Esmilna <esmilnaramirez2107@gmail.com>
2023-03-04 20:16:10 -0400

    Fue creada la pantalla del detalle de producto

--- ClientApp/src/components/ProductDetails/ProductDetails.jsx
+++ ClientApp/src/components/ProductDetails/ProductDetails.jsx
@@ -12,57 +12,115 @@ import {
     CardBody,
     CardText,
     CardTitle,
-    CardSubtitle
+    CardSubtitle,
+    Container,
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    UncontrolledDropdown,
 } from 'reactstrap';
 
 function ProductDetails() {
 
     return (
         <div>
-            <h1>Dashboard</h1>
+            <h1>Product Details</h1>
 
-            <div>
-
-                <Card
-                    style={{
-                        width: '18rem'
-                    }}
-                >
-                    <img
-                        alt="Sample"
-                        src="https://picsum.photos/300/200"
-                    />
-                    <CardBody>
-                        <CardTitle tag="h5">
-                            Card title
-                        </CardTitle>
-                        <CardSubtitle
-                            className="mb-2 text-muted"
-                            tag="h6"
-                        >
-                            Card subtitle
-                        </CardSubtitle>
-                        <CardText>
-                            Some quick example text to build on the card title and make up the bulk of the card‘s content.
-                        </CardText>
-                        <Button>
-                            Button
-                        </Button>
-                    </CardBody>
-                </Card>
-
-
-            </div>
-           
-            <div class="row">
-                <div class="col col-md-6 img-size">
-                    <img src="https://i.pinimg.com/originals/6e/f5/6f/6ef56f8f50728b314d9dd174804de445.jpg" />
-                </div>
-                <div class="col col-md-6 img-size">
-                    <img src="https://i.pinimg.com/originals/6e/f5/6f/6ef56f8f50728b314d9dd174804de445.jpg" />
-                </div>
+            <Container>
                 
-            </div>
+                <Row>
+                    <Col className="bg-light">
+                        <div>
+                        <img className="imagenPrincipal" src="https://www.elcarrocolombiano.com/wp-content/uploads/2021/02/20210208-TOP-75-CARROS-MAS-VENDIDOS-DE-COLOMBIA-EN-ENERO-2021-01.jpg"/>
+                            </div>
+                         <Row>
+                            <Col className="bg-light" md="3">
+                                <div>
+                                <img className="tamanoImagen margenIzquierdo margenSuperior" src="https://www.elcarrocolombiano.com/wp-content/uploads/2021/02/20210208-TOP-75-CARROS-MAS-VENDIDOS-DE-COLOMBIA-EN-ENERO-2021-01.jpg" />
+                                </div>
+                            </Col>
+                            <Col className="bg-light" md="3">
+                                <div>
+                                    <img className="tamanoImagen margenIzquierdo margenSuperior" src="https://www.elcarrocolombiano.com/wp-content/uploads/2021/02/20210208-TOP-75-CARROS-MAS-VENDIDOS-DE-COLOMBIA-EN-ENERO-2021-01.jpg" />
+                                </div>
+                            </Col>
+                            <Col className="bg-light" md="3">
+                                <div>
+                                    <img className="tamanoImagen margenIzquierdo margenSuperior" src="https://www.elcarrocolombiano.com/wp-content/uploads/2021/02/20210208-TOP-75-CARROS-MAS-VENDIDOS-DE-COLOMBIA-EN-ENERO-2021-01.jpg" />
+                                </div>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col className="bg-light border">
+                        <h4 className="nombreProductoMargin"><strong>Nombre del producto</strong></h4>
+                        
+                        <Row>
+                            <Col className="bg-light" md="3">
+                                <p className="precioVenta"><strong>$500.00</strong></p>
+                            </Col>
+                            <Col className="bg-light" md="2">
+                                <p className="precioOferta"><strong>$350.00</strong></p>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <p className="margenIzquierdo margenSuperior">Color: <strong>White</strong></p>
+                        </Row>
+
+                        <Row>
+                            <Col className="bg-light" md="3">
+                                <img className="tamanoImagen margenIzquierdo" src="https://www.elcarrocolombiano.com/wp-content/uploads/2021/02/20210208-TOP-75-CARROS-MAS-VENDIDOS-DE-COLOMBIA-EN-ENERO-2021-01.jpg" />
+                            </Col>
+                            <Col className="bg-light" md="3">
+                                <img className="tamanoImagen margenIzquierdo" src="https://www.elcarrocolombiano.com/wp-content/uploads/2021/02/20210208-TOP-75-CARROS-MAS-VENDIDOS-DE-COLOMBIA-EN-ENERO-2021-01.jpg" />
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col className="bg-light margenIzquierdo marginSuperiorBotones" md="3">
+                                <UncontrolledDropdown group>
+                                    <Button color="light" className="Borde">
+                                        Primary
+                                    </Button>
+                                    <DropdownToggle
+                                        caret
+                                        color="light"
+                                    />
+                                    <DropdownMenu>
+                                        <DropdownItem header>
+                                            Header
+                                        </DropdownItem>
+                                        <DropdownItem>
+                                            Some Action
+                                        </DropdownItem>
+                                        
+                                    </DropdownMenu>
+                                </UncontrolledDropdown>
+                            </Col>
+
+                            <Col className="bg-light margenIzquierdo marginSuperiorBotones" md="3">
+                                <Button color="dark" className="Borde">
+                                    Add to Car
+                                </Button>
+                                {' '}
+                            </Col>
+
+                            <Col className="bg-light margenIzquierdo marginSuperiorBotones" md="3">
+                                <Button outline color="dark" className="Borde" >
+                                    WishList
+                                </Button>
+                                {' '}
+                            </Col>
+                        </Row>                        
+
+                    </Col>
+                    
+                </Row>
+               
+            </Container>
+           
+            
             
         </div>
 
